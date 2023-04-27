75c13958bbf2a9c40c42f8f600522eba85127cf2
K2adir <k2adir@gmail.com>
2023-04-27 15:13:29 -0400

    nav css
    nav css added, basic file structrure. sass installed

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Navigation from "./components/navigation/Navigation";
+
+function App() {
+  return (
+    <main className="App">
+      <Navigation />
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/components/navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import "./Navigation.scss";
+
+const Navigation = () => {
+  return (
+    <main>
+      <nav className="nav_container">
+        <a href="" className="logo">
+          Blog
+        </a>
+        <div className="nav_links">
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </div>
+      </nav>
+    </main>
+  );
+};
+
+export default Navigation;
