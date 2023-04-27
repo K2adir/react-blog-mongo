4ccb2b7515d8a7b6e28076923678aa0a5f835485
K2adir <k2adir@gmail.com>
2023-04-27 15:27:59 -0400

    post layout ok

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
-import Navigation from "./components/navigation/Navigation";
+import Navigation from "./components/Navigation/Navigation";
+import Posts from "./components/Posts/Posts";
 
 function App() {
   return (
     <main className="App">
       <Navigation />
+      <Posts />
     </main>
   );
 }
---
+++ client/src/components/Posts/Posts.jsx
@@ -0,0 +1,18 @@
+import "./Posts.scss";
+
+const Posts = () => {
+  return (
+    <div className="post_container">
+      <img src="https://picsum.photos/275/150" alt="" />
+      <div className="post_title">
+        <h1>Hello Post</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Est libero
+          nulla ex fuga iure totam quis, corrupti enim quam placeat.{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
