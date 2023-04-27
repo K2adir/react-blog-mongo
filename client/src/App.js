130396f956993180d3eb89f9a281e9ff64952d7e
K2adir <k2adir@gmail.com>
2023-04-27 16:22:44 -0400

    app setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import "./App.scss";
 import Navigation from "./components/Navigation/Navigation";
 import Post from "./components/Post/Post";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <main className="App">
+      <Routes>
+        {/* // */}
+        {/* // */}
+      </Routes>
       <Navigation />
       <Post />
     </main>
