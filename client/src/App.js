4fa66f50863721394cc480604b38a1e1c56bbc26
K2adir <k2adir@gmail.com>
2023-04-28 13:30:52 -0400

    Context.Provider
    user context OK

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,27 @@ import Post from "./components/Post/Post";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/LoginRegisterForm/Login";
 import Register from "./components/LoginRegisterForm/Register";
-
+import { UserContextProvider } from "./Context/UserContext";
 function App() {
   return (
-    <main className="App">
-      <Navigation />
-      <Routes>
-        <Route
-          index
-          element={
-            <>
-              <Post />
-              <Post />
-            </>
-          }
-        />
-        <Route path={"/login"} element={<Login />} />
-        <Route path={"/register"} element={<Register />} />
-      </Routes>
-    </main>
+    <UserContextProvider>
+      <main className="App">
+        <Navigation />
+        <Routes>
+          <Route
+            index
+            element={
+              <>
+                <Post />
+                <Post />
+              </>
+            }
+          />
+          <Route path={"/login"} element={<Login />} />
+          <Route path={"/register"} element={<Register />} />
+        </Routes>
+      </main>
+    </UserContextProvider>
   );
 }
 
