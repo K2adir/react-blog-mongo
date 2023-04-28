6b9d261f55c33a4d1f24a664105487802276c0b0
K2adir <k2adir@gmail.com>
2023-04-28 14:35:24 -0400

    React Quill installed - fields Working
    all field are working, working on image upload now.

--- client/src/pages/NewPost.jsx
+++ client/src/pages/NewPost.jsx
@@ -1,11 +1,64 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { Form } from "react-router-dom";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+/// React-Quill module -> from their npm
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+];
+///
 const NewPost = () => {
+  const [title, setTitle] = useState("");
+  const [summary, setSummary] = useState("");
+  const [content, setContent] = useState("");
+
   return (
-    <div>
-      <h2>New Post</h2>
-      <h2>New Post</h2>
-    </div>
+    <form>
+      <input
+        type="title"
+        placeholder={"Title"}
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="summary"
+        placeholder={"Summary"}
+        value={summary}
+        onChange={(e) => setSummary(e.target.value)}
+      />
+      <input type="file" />
+      <ReactQuill
+        value={content}
+        modules={modules}
+        formats={formats}
+        onChange={(newValue) => setContent(newValue)}
+      />
+      <button>Create Post</button>
+    </form>
   );
 };
 
