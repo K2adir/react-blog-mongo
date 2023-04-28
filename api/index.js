5268212bfc0cfea5352f0ed041ce66b51d27c169
K2adir <k2adir@gmail.com>
2023-04-27 22:59:13 -0400

    LOGIN OK -
    user can login, MONGO checks the password and warns for problems

--- api/index.js
+++ api/index.js
@@ -8,11 +8,13 @@ const PORT = 4500;
 const User = require("./models/User");
 const { create } = require("./models/User");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 ////
 var salt = bcrypt.genSaltSync(10);
+const secret = process.env.JTW_PASS;
 ////
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 
 // version error handling thingy.
@@ -41,6 +43,24 @@ app.post("/register", async (req, res) => {
     res.json(userDoc);
   } catch (e) {
     res.status(400).json(e);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  const userDoc = await User.findOne({ username });
+  //   res.json(userDoc);   CHECKS THE PAYLOAD
+  const passCorrect = bcrypt.compareSync(password, userDoc.password);
+  //   res.json(passCorrect); CHECKS PASS - TRUE / FALSE on Payload
+
+  // if login is success, network/headers will display set-cookie
+  if (passCorrect) {
+    jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
+      if (err) throw err;
+      res.cookie("token", token).json("ok");
+    });
+  } else {
+    res.status(400).json("check your username or pass");
   }
 });
 
