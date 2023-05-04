eb30bb4537d0712770011535ad829c03ab09a9a0
K2adir <k2adir@gmail.com>
2023-05-04 11:14:47 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.use(
     credentials: true,
     origin: [
       "http://localhost:3000",
-      //   "https://mern-stuff-stuff-app.onrender.com",
+      "https://mern-stuff-stuff-app.onrender.com",
     ],
   })
 );
